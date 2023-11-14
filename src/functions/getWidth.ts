065c5886f6ce5ec45aa89114d75a54abaacc1165
omar-alghaish <omaralghish@gmail.com>
2023-11-14 21:30:39 +0200

    some changes

--- src/functions/getWidth.ts
+++ src/functions/getWidth.ts
@@ -2,8 +2,7 @@ function getVisibleWidth(str: String) {
   const colorCodeRegex = /\x1b\[(38;2;\d{1,3};\d{1,3};\d{1,3}m|0m)/g; // Regular expression to match ANSI color codes
   const visibleText = str.replace(colorCodeRegex, ""); // Remove color codes
   const width = visibleText.length;
-  return width ;
+  return width;
 }
 
-
-export default getVisibleWidth
+export default getVisibleWidth;
