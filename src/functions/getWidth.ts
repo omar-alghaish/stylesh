7018d8383e05e40f6e1b6436fa7f23127a6f5a99
omar-alghaish <omaralghish@gmail.com>
2023-11-16 20:22:41 +0200

    some changes

--- src/functions/getWidth.ts
+++ src/functions/getWidth.ts
@@ -1,6 +1,6 @@
 function getVisibleWidth(str: String) {
-  const colorCodeRegex = /\x1b\[(38;2;\d{1,3};\d{1,3};\d{1,3}m|0m)/g; // Regular expression to match ANSI color codes
-  const visibleText = str.replace(colorCodeRegex, ""); // Remove color codes
+  const colorCodeRegex = /\x1b\[(38;2;\d{1,3};\d{1,3};\d{1,3}m|0m)/g; 
+  const visibleText = str.replace(colorCodeRegex, ""); 
   const width = visibleText.length;
   return width;
 }
